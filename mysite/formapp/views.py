ac0d3cdd05c1c5658157845d197110b18eccad69
Jenny <jennyhoang_9@hotmail.com>
2025-04-28 09:41:55 -0400

    Form app

---
+++ mysite/formapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+# Create your views here.
+def home_view(request):
+    return render(request, 'formapp/home.html')
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.send_email()
+            return redirect('contact-success')
+    else:
+        form = ContactForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'formapp/contact.html', context)
+
+def contact_success_view(request):
+    return render(request, 'formapp/contact_success.html')
