69a6c2d30241c1a541a3afbbef4da9735e144ee8
Jenny <jennyhoang_9@hotmail.com>
2025-05-04 14:16:46 -0400

    missing coma

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -137,7 +137,7 @@ LOGIN_REDIRECT_URL = 'home'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 sentry_sdk.init(
-    dsn=os.getenv('SENTRY_DSN')
+    dsn=os.getenv('SENTRY_DSN'),
     # Add data like request headers and IP for users,
     # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
     send_default_pii=True,
